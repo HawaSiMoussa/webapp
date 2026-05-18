a54e713cc25636b4825dfb9e824000fcb9280276
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-05-18 21:14:06 +0200

    added flask app

--- docs/app.py
+++ docs/app.py
@@ -1,9 +1,31 @@
 from flask import Flask
-app= Flask(__name__)
 
-@app.route ("/")
+app = Flask(__name__)
+
+title = "LostAndFound"
+group = "CampusFinder"
+
+members = [
+    "77204183234, Hawa Si Moussa",
+    "77209887107, Fatme Berjaoui",
+    "77209886771, Sarah Tayem"
+]
+
+ tasks= [
+    "Hawa: Benutzeroberfläche Login button posts Darstellung mit HTML, CSS",
+    "Fatme: Git hub Repo's ( pull commit push und aufgaben zusammenfügen), Organisieren/dokumentieren der verschiedenen Ideen"
+    "Sarah: Datenbankverwaltung (Nutzer und Beiträge), sowie Analyse geplanter Funktionen bezüglich technischer Machbarkeit und Aufwand"
+]
+
+@app.route("/")
 def home():
-    return ("Lost & Found")
+    return render_template(
+        "index.html",
+        title=title,
+        group=group,
+        members=members,
+        tasks=tasks
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
