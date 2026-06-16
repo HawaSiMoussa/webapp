baca50a5e1d78411720a6831ea80637171e98fc3
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-06-16 23:42:25 +0200

    RemoveBrackets

--- docs/forms.py
+++ docs/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import InputRequired, Length
 
 #Post erstellen
 class PostForm(FlaskForm):
-    title = StringField("Titel")(validators=[InputRequired()])
-    description = TextAreaField("Beschreibung")(validators=[InputRequired()])
-    lost_date = DateField("Verlustdatum")(validators=[InputRequired()])
-    lost_area = StringField("Verlustort")(validators=[InputRequired()])
+    title = StringField("Titel", validators=[InputRequired()])
+    description = TextAreaField("Beschreibung", validators=[InputRequired()])
+    lost_date = DateField("Verlustdatum", validators=[InputRequired()])
+    lost_area = StringField("Verlustort", validators=[InputRequired()])
     submit = SubmitField("Veröffentlichen")
