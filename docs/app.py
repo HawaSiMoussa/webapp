ab60ee7886dfefd6e361a6d27cd55835fd84a8c6
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-06-17 01:29:43 +0200

    update html and app.py (add new functions and posts)

--- docs/app.py
+++ docs/app.py
@@ -1,7 +1,6 @@
-import os
 
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5  # (1.)
+from flask_bootstrap import Bootstrap5  
 
 app = Flask(__name__)
 
@@ -9,7 +8,32 @@ bootstrap = Bootstrap5(app)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+
+    posts = [
+        {
+            "username": "Sarah",
+            "title": "Schwarzer Rucksack",
+            "views": 12,
+            "DateOfLoss": "2024-06-01",
+        },
+        {
+            "username": "Max",
+            "title": "Laptop",
+            "views": 5,
+            "DateOfLoss": "2024-04-01",
+        },
+        {
+            "username": "Laura",
+            "title": "Schlüsselbund",
+            "views": 8,
+            "DateOfLoss": "2024-05-01",
+        }
+    ]
+
+    return render_template(
+        "home.html",
+        posts=posts
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
