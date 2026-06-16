3218a7acdbda496f3b47c06147f32ec036092c09
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-06-16 22:04:07 +0200

    1.2 Kontaktformular

---
+++ docs/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, TextAreaField, DateField, SubmitField
+from wtforms.validators import InputRequired, Length  
+
+#Post erstellen
+class PostForm(FlaskForm):
+    title = StringField("Titel")(validators=[InputRequired()])
+    description = TextAreaField("Beschreibung")(validators=[InputRequired()])
+    lost_date = DateField("Verlustdatum")(validators=[InputRequired()])
+    submit = SubmitField("Veröffentlichen")
