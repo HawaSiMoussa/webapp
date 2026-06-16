6d4960040695858c57384a4555399821eb6de2fb
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-06-16 23:26:49 +0200

    AddLost_area

--- docs/forms.py
+++ docs/forms.py
@@ -7,4 +7,5 @@ class PostForm(FlaskForm):
     title = StringField("Titel")(validators=[InputRequired()])
     description = TextAreaField("Beschreibung")(validators=[InputRequired()])
     lost_date = DateField("Verlustdatum")(validators=[InputRequired()])
+    lost_area = StringField("Verlustort")(validators=[InputRequired()])
     submit = SubmitField("Veröffentlichen")
