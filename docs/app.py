3a99926362460106e0e2ef897c9e9862f23d3737
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-06-16 23:47:50 +0200

    home html, app.py und model.py

--- docs/app.py
+++ docs/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, redirect, url_for
+from requests import post
+from flask_bootstrap import Bootstrap5  # (1.)
+import db, forms
 
 app = Flask(__name__)
-@app.route("/")
-def home():
-    return "Flask funktioniert!"
+
+bootstrap = Bootstrap5(app)
 
 @app.route("/")
-def home():
+def index():
     return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
---
+++ docs/model.py
@@ -0,0 +1,16 @@
+
+from httpx import post
+
+
+class Post(db.Model):
+
+    title = post.title
+    description = post.description
+    views = post.views
+    username = user.username
+    createDate = post.createDate
+    expireDate = post.expireDate
+    locationOfLoss = post.locationOfLoss
+    dateOfLoss = post.dateOfLoss
+    status= post.status
+    mail = user.hwrMail
