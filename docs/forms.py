b1b0bc4d31e365eda89225e9eda09a9ab82c1037
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-06-17 00:34:45 +0200

    AddClassContactForm

--- docs/forms.py
+++ docs/forms.py
@@ -2,6 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, TextAreaField, DateField, SubmitField
 from wtforms.validators import InputRequired, Length  
 
+class ContactForm(FlaskForm):
+    name = StringField("Name", validators=[InputRequired()])
+    username = StringField("Benutzername", validators=[InputRequired()])
+    phone_number = StringField("Telefonnummer", validators=[InputRequired()])                     
+    submit = SubmitField("Registrieren")
+
 #Post erstellen
 class CreatePostForm(FlaskForm):
     title = StringField("Titel", validators=[InputRequired()])
