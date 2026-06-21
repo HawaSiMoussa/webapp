3ee09fffce67ec20fe0e8b779484f01e6ae4cebc
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-06-21 19:07:20 +0200

    remove old code

--- app.py
+++ app.py
@@ -16,32 +16,6 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-title = "LostAndFound"
-group = "CampusFinder"
-
-members = [
-    "77204183234, Hawa Si Moussa",
-    "77209887107, Fatme Berjaoui",
-    "77209886771, Sarah Tayem"
-]
-
-tasks = [
-    "Hawa: Benutzeroberfläche Login button posts Darstellung mit HTML, CSS",
-    "Fatme: Git hub Repo's ( pull commit push und aufgaben zusammenfügen), Organisieren/dokumentieren der verschiedenen Ideen",
-    "Sarah: Datenbankverwaltung (Nutzer und Beiträge), sowie Analyse geplanter Funktionen bezüglich technischer Machbarkeit und Aufwand"
-]
-
-
-@app.route("/")
-def index():
-    return render_template(
-        "index.html",
-        title=title,
-        group=group,
-        members=members,
-        tasks=tasks
-    )
-
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
