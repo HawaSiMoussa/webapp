7bb0daf096b6ff77ea8995be171dc4a33c752e9c
Hawa Si Moussa <s_simoussah24@stud.hwr-berlin.de>
2026-06-21 05:20:40 +0200

    datenbank für login

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key_just_for_dev_environment'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///lostandfound.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
- 
+
 bootstrap = Bootstrap5(app)
 db.init_app(app)
 with app.app_context():
     db.create_all()
-     
+    
 
 title = "LostAndFound"
 group = "CampusFinder"
@@ -30,7 +30,6 @@ tasks= [
     "Fatme: Git hub Repo's ( pull commit push und aufgaben zusammenfügen), Organisieren/dokumentieren der verschiedenen Ideen",
     "Sarah: Datenbankverwaltung (Nutzer und Beiträge), sowie Analyse geplanter Funktionen bezüglich technischer Machbarkeit und Aufwand"
 ]
-
 @app.route("/")
 def index():
     return render_template(
@@ -40,24 +39,21 @@ def index():
         members=members,
         tasks=tasks
     )
-
 import os
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap5
 from db import db, Post, StandardUser
-import forms 
-
+import forms
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret_key_just_for_dev_environment'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///lostandfound.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
- 
+
 bootstrap = Bootstrap5(app)
 db.init_app(app)
 with app.app_context():
     db.create_all()
-     
 
 title = "LostAndFound"
 group = "CampusFinder"
@@ -109,30 +105,27 @@ flash("Account erstellt!", "success")
 return redirect(url_for("login"))
 return render_template("register.html",form=forms)
 
-
-
 @app.route('/logins/', methods=['GET', 'POST'])
 def login():
-
-form = forms.CreateLogin()  # (1.)
+    form = forms.CreateLogin()  # (1.)
 if forms.validate_on_submit():
     user = db.session.execute(
 db.select(StandardUser).where(StandardUser.hwr_mail == forms.hwrmail.data)
 
     ).scalar_one_or_none()
-if not user: 
+if not user:
         flash ("User existiert nicht", "warning")
         return redirect(url_for("login"))
-    
+
 if user.passwort != form.passwort.data:
         flash ("Falsches Passwort!","warning")
         return redirect(url_for("login"))
-    
+
         flash("Login erfolgreich!","success")
         return redirect(url_for("index"))
 
-        return render_template("login-html", form=forms)                 
-      
+        return render_template("login-html", form=forms)
+
 @app.route('/suche/', methods=['GET', 'POST'])
 def suche():
     form = forms.Suchleiste()
@@ -174,7 +167,6 @@ def suche():
     form = forms.Suchleiste()
 
     posts=[]
-        
 
     if form.validate_on_submit():
         suchwort = form.suchbegriff.data
@@ -191,7 +183,7 @@ def suche():
             form=form,
             posts=[]
         )
-
+    
     if form.validate_on_submit():
         suchwort = form.suchbegriff.data
 
@@ -200,7 +192,6 @@ def suche():
                 Post.titel.ilike(f"%{suchwort}%")
             )
         ).scalars().all()
-
 
     return render_template(
             'suche.html',
