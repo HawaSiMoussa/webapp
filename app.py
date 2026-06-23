c88a0408a2283552ec44981a95b808fe78a13ede
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-06-23 03:58:47 +0200

    update home route to return a single post instead of multiple

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def home():
     user = db.session.get(StandardUser,session["user_id"])
 
     posts = db.session.execute(db.select(Post).where(Post.ablaufdatum>=date.today(), Post.status == "laufend")
-                               ).scalars()
+                               ).one()
     return render_template("home.html", posts=posts, form=form, user=user)
 
 
